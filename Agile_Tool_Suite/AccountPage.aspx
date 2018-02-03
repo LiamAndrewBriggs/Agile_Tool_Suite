81a785c25ea040e54cbfe5ab6a2413506874981d
Liam Briggs <liamandrewbriggs@gmail.com>
2018-02-03 01:20:04 +0000

    Master Page
    SideBar added to a master page test account page added alsp

---
+++ Agile_Tool_Suite/AccountPage.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master.Master" AutoEventWireup="true" CodeBehind="AccountPage.aspx.cs" Inherits="Agile_Tool_Suite.AccountPage" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="title" runat="server">
+    Account
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="head" runat="server">
+    <link href="Css/Account.css" rel="stylesheet" />
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <p> Test </p>
+</asp:Content>
---
+++ Agile_Tool_Suite/Master.Master.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Agile_Tool_Suite
+{
+    public partial class Master : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void LogoutMethod(object sender, EventArgs e)
+        {
+            Session["uname"] = null;
+            Session.Abandon();
+            Response.BufferOutput = true;
+            Response.Redirect("Home.aspx", false);
+        }
+    }
+}
